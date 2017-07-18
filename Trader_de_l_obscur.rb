eab715e165db59c6898c42343e2d808921986729
Fadilmb <fadil.boodoo@etu.upmc.fr>
2017-07-18 18:16:48 +0200

    done

--- Trader_de_l_obscur.rb
+++ Trader_de_l_obscur.rb
@@ -3,25 +3,26 @@ require 'nokogiri'
 require 'open-uri'
 
 def get_the_curencies_of_CryptoCurrency
-		names = Array.new
-		prices = Array.new
-		page = Nokogiri::HTML(open('https://coinmarketcap.com/all/views/all/'))
-		names = page.css('td.no-wrap.currency-name')
-		prices = page.css('a.price')
-		#for i in (0..names.length-1)
-			#names[i] = names[i].text
-			#names[k] = page.css('a.price').text
-		#}
-		#end
-		names.each{|k, name|
-			names[k] = name.text
-		}
-		puts names
-		#puts names[4].text
-		#puts prices
+	names = Array.new 
+	names_text = Array.new
+	prices = Array.new 
+	prices_text = Array.new
+	names_with_price = Hash.new
+
+	page = Nokogiri::HTML(open('https://coinmarketcap.com/all/views/all/'))
+	names = page.css('td.no-wrap.currency-name')
+	names.each {|name| names_text.push(name.text.gsub(" ",""))}
+	prices = page.css('a.price')
+	prices.each { |price| prices_text.push(price.text)}
 	
-	                                                                                       
+	for i in (0..names_text.length-1)
+		names_with_price[names_text[i]] = prices_text[i]
+	end
+
+	return names_with_price                                                                  
 end
-#td.no-wrap.currency-name
-#a.price
-get_the_curencies_of_CryptoCurrency
+
+while true
+	puts get_the_curencies_of_CryptoCurrency
+	sleep 3600
+end
